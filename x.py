ee3600df5bb8a188df1494106d0f28eec1e3c500
ntkntk123 <nguyentrongkhai13022005@gmail.com>
2025-06-23 19:37:53 +0700

    Update x.py

--- x.py
+++ x.py
@@ -1,39 +1,48 @@
-try:
- import os, sys, random
- from time import sleep
- from telethon import TelegramClient, sync
- from telethon.errors import SessionPasswordNeededError, FloodWaitError
- from telethon.tl.functions.messages import  GetHistoryRequest
-except:
- os.system("pip install random")
- os.system("pip install telethon")
- import os, sys, random
- from time import sleep
- from telethon import TelegramClient, sync
- from telethon.errors import SessionPasswordNeededError, FloodWaitError
- from telethon.tl.functions.messages import GetHistoryRequest
-print("MUA ACC TELEGRAM IB @Longsbkt") 
-while True:
-    
-           
-        phone = input("Nhap So Dien Thoai:")
-        if phone == 'xx':
-            os.system('clear')
-            break
-        else:
-            api_id = 21647330
-            api_hash = '7591f9c230e678835e8f82a13dd31e3e'
-            client = TelegramClient("sessions/"+phone,api_id,api_hash)
-            client.connect()
-            if not client.is_user_authorized():
-                print (F"Session lỗi!" + phone)
-                #client.log_out()
+from flask import Flask, request, jsonify
+import os
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.messages import GetHistoryRequest
+
+app = Flask(__name__)
+
+# Telegram App credentials
+api_id = 21647330
+api_hash = '7591f9c230e678835e8f82a13dd31e3e'
+
+@app.route('/get_code', methods=['GET'])
+def get_code():
+    phone = request.args.get('sdt')
+
+    if not phone:
+        return jsonify({'error': 'Thiếu số điện thoại (sdt)'}), 400
+
+    # Save session file in the current folder
+    session_path = phone
+
+    client = TelegramClient(session_path, api_id, api_hash)
+
+    try:
+        client.connect()
+
+        if not client.is_user_authorized():
+            client.disconnect()
+            return jsonify({'error': f'Session lỗi với số {phone}'}), 401
+
+        messages = client.get_messages(777000, limit=1)
+        for message in messages:
+            msg = message.message
+            if "code" in msg.lower():
+                you_code = msg.split()[2].rstrip('.')
                 client.disconnect()
-                continue
-            else:
-                for message in client.get_messages(777000, limit=1):
-                    msg = message.message
-                    you_code = msg.split()[2].rstrip('.')
-                    print ("Code =>> "+you_code)
-                    client.disconnect()    
-  
+                return jsonify({'sdt': phone, 'code': you_code})
+
+        client.disconnect()
+        return jsonify({'error': 'Không tìm thấy mã xác thực trong tin nhắn'}), 404
+
+    except Exception as e:
+        client.disconnect()
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
